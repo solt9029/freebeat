e3663d1b972077bf53f9f1f01672b9262ab41bfa
Kenshi Shiode <atemu1212@yahoo.co.jp>
2021-07-02 23:45:56 +0900

    Add api

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import { AppBar, Toolbar, Typography } from '@material-ui/core'
 
-const IndexPage = () => (
-  <AppBar position="static">
-    <Toolbar>
-      <Typography variant="h6">FreeBeat</Typography>
-    </Toolbar>
-  </AppBar>
-)
+const IndexPage = () => {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6">FreeBeat</Typography>
+      </Toolbar>
+    </AppBar>
+  )
+}
 
 export default IndexPage
