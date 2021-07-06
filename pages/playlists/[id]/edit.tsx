39f040fb23d98edf243ecc7b9c5b0bdadb35d55f
Kenshi Shiode <atemu1212@yahoo.co.jp>
2021-07-06 21:23:00 +0900

    Add form

--- pages/playlists/[id]/edit.tsx
+++ pages/playlists/[id]/edit.tsx
@@ -10,7 +10,7 @@ const useStyles = makeStyles((theme) => ({
     '&::before': {
       content: '""',
       display: 'block',
-      paddingTop: '56.25%',
+      paddingTop: '60%',
     },
   },
   content: {
@@ -19,6 +19,12 @@ const useStyles = makeStyles((theme) => ({
     left: 0,
     bottom: 0,
     right: 0,
+  },
+  form: {
+    width: '100%',
+  },
+  formInput: {
+    fontSize: '1.5rem',
   },
 }))
 
@@ -37,26 +43,36 @@ const EditPage = () => {
   console.log(loading)
 
   return (
-    <Box mt={10} mx={5}>
+    <Box mt={5}>
       <Container>
-        <Grid spacing={10} container>
-          <Grid item sm={6} md={4} className={classes.wrapper}>
-            {/* <div className={classes.content}> */}
-            <YouTube
-              containerClassName={classes.content}
-              opts={{
-                height: '100%',
-                width: '100%',
-                playerVars: {
-                  playsinline: 1,
-                },
-              }}
-              videoId="eqr98KBmsJk"
-            />
+        <Grid spacing={3} container>
+          <Grid item xs={12} sm={6} md={4}>
+            <div className={classes.wrapper}>
+              <YouTube
+                containerClassName={classes.content}
+                opts={{
+                  height: '100%',
+                  width: '100%',
+                  playerVars: {
+                    playsinline: 1,
+                  },
+                }}
+                videoId="eqr98KBmsJk"
+              />
+            </div>
+
             {/* </div> */}
           </Grid>
-          <Grid item sm={6} md={8}>
-            <TextField label="Standard" />
+          <Grid item xs={12} sm={6} md={8}>
+            <TextField
+              className={classes.form}
+              InputProps={{
+                classes: {
+                  input: classes.formInput,
+                },
+              }}
+              label="タイトル"
+            />
           </Grid>
         </Grid>
 
