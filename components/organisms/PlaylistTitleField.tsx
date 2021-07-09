b2fb340f07861fe6d99233aee375c392b818ce5b
Kenshi Shiode <atemu1212@yahoo.co.jp>
2021-07-09 20:48:30 +0900

    Add handleChange

--- components/organisms/PlaylistTitleField.tsx
+++ components/organisms/PlaylistTitleField.tsx
@@ -1,10 +1,9 @@
-import { makeStyles, TextField } from '@material-ui/core'
-import React from 'react'
+import { debounce, makeStyles, TextField } from '@material-ui/core'
+import { useRouter } from 'next/dist/client/router'
+import React, { useCallback, useState } from 'react'
+import { usePlaylistQuery } from '../../graphql/generated/graphql-client'
 
 const useStyles = makeStyles(() => ({
-  field: {
-    width: '100%',
-  },
   input: {
     fontSize: '1.5rem',
   },
@@ -13,9 +12,42 @@ const useStyles = makeStyles(() => ({
 function PlaylistTitleField() {
   const classes = useStyles()
 
+  const [title, setTitle] = useState('')
+
+  const {
+    query: { id },
+    isReady,
+  } = useRouter()
+
+  usePlaylistQuery({
+    variables: { id: parseInt(id?.toString()) },
+    onCompleted: (data) => {
+      setTitle(data.playlist.title)
+    },
+    skip: !isReady,
+  })
+
+  const updateTitle = useCallback(
+    debounce((title: string) => {
+      // update title
+      console.log(title)
+    }, 1000),
+    [],
+  )
+
+  const handleChange = useCallback(
+    (event) => {
+      setTitle(event.target.value)
+      updateTitle(event.target.value)
+    },
+    [setTitle, updateTitle],
+  )
+
   return (
     <TextField
-      className={classes.field}
+      onChange={handleChange}
+      fullWidth
+      value={title}
       InputProps={{
         classes: {
           input: classes.input,
