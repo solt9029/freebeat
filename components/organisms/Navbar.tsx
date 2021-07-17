6cbd3e2cb91406a847467bcf26f186d2ba18df29
Kenshi Shiode <atemu1212@yahoo.co.jp>
2021-07-17 22:13:16 +0900

    Add search

--- components/organisms/Navbar.tsx
+++ components/organisms/Navbar.tsx
@@ -1,17 +1,88 @@
-import { AppBar, Container, Toolbar, Typography } from '@material-ui/core'
+import {
+  alpha,
+  AppBar,
+  Container,
+  createStyles,
+  InputBase,
+  makeStyles,
+  Toolbar,
+  Typography,
+} from '@material-ui/core'
 import React from 'react'
 import Link from 'next/link'
+import { Search } from '@material-ui/icons'
+import Theme from '../../theme'
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    search: {
+      position: 'relative',
+      borderRadius: theme.shape.borderRadius,
+      backgroundColor: alpha(theme.palette.common.white, 0.15),
+      '&:hover': {
+        backgroundColor: alpha(theme.palette.common.white, 0.25),
+      },
+      marginLeft: 0,
+      width: '100%',
+      [theme.breakpoints.up('sm')]: {
+        marginLeft: theme.spacing(1),
+        width: 'auto',
+      },
+    },
+    searchIcon: {
+      padding: theme.spacing(0, 1),
+      height: '100%',
+      position: 'absolute',
+      pointerEvents: 'none',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    inputRoot: {
+      color: 'inherit',
+    },
+    inputInput: {
+      padding: theme.spacing(1, 1, 1, 0),
+      paddingLeft: `calc(1em + ${theme.spacing(3)}px)`,
+      transition: theme.transitions.create('width'),
+      width: '100%',
+      [theme.breakpoints.up('sm')]: {
+        width: '20ch',
+        '&:focus': {
+          width: '20ch',
+        },
+      },
+    },
+  }),
+)
 
 function Navbar() {
+  const classes = useStyles()
+
   return (
     <AppBar position="static">
-      <Toolbar>
-        <Link href="/">
-          <Container style={{ cursor: 'pointer' }}>
-            <Typography variant="h6">FreeBeat</Typography>
-          </Container>
-        </Link>
-      </Toolbar>
+      <Container fixed>
+        <Toolbar>
+          <Link href="/">
+            <Container style={{ cursor: 'pointer' }}>
+              <Typography variant="h6">FreeBeat</Typography>
+            </Container>
+          </Link>
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <Search />
+            </div>
+            <InputBase
+              placeholder="検索"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </div>
+        </Toolbar>
+      </Container>
     </AppBar>
   )
 }
