cd7f45619b56d6189f94088293d33ee8c3572230
Kenshi Shiode <atemu1212@yahoo.co.jp>
2021-07-17 23:00:44 +0900

    Add SearchField

--- components/organisms/Navbar.tsx
+++ components/organisms/Navbar.tsx
@@ -1,64 +1,9 @@
-import {
-  alpha,
-  AppBar,
-  Container,
-  createStyles,
-  InputBase,
-  makeStyles,
-  Toolbar,
-  Typography,
-} from '@material-ui/core'
+import { AppBar, Container, Toolbar, Typography } from '@material-ui/core'
 import React from 'react'
 import Link from 'next/link'
-import { Search } from '@material-ui/icons'
+import SearchField from './SearchField'
 
-const useStyles = makeStyles((theme) =>
-  createStyles({
-    search: {
-      position: 'relative',
-      borderRadius: theme.shape.borderRadius,
-      backgroundColor: alpha(theme.palette.common.white, 0.15),
-      '&:hover': {
-        backgroundColor: alpha(theme.palette.common.white, 0.25),
-      },
-      marginLeft: 0,
-      width: '100%',
-      [theme.breakpoints.up('sm')]: {
-        marginLeft: theme.spacing(1),
-        width: 'auto',
-      },
-    },
-    searchIcon: {
-      padding: theme.spacing(0, 1),
-      height: '100%',
-      position: 'absolute',
-      pointerEvents: 'none',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    inputRoot: {
-      color: 'inherit',
-    },
-    inputInput: {
-      padding: theme.spacing(1, 1, 1, 0),
-      paddingLeft: `calc(1em + ${theme.spacing(3)}px)`,
-      transition: theme.transitions.create('width'),
-      width: '100%',
-      [theme.breakpoints.up('sm')]: {
-        width: '20ch',
-        '&:focus': {
-          width: '20ch',
-        },
-      },
-    },
-  }),
-)
-
-// 検索キーワードを入れたら、debounceでrouter.pushする。そのキーワードに応じてplaylists/indexで出しわけ
 function Navbar() {
-  const classes = useStyles()
-
   return (
     <AppBar position="static">
       <Container fixed>
@@ -68,19 +13,7 @@ function Navbar() {
               <Typography variant="h6">FreeBeat</Typography>
             </Container>
           </Link>
-          <div className={classes.search}>
-            <div className={classes.searchIcon}>
-              <Search />
-            </div>
-            <InputBase
-              placeholder="検索"
-              classes={{
-                root: classes.inputRoot,
-                input: classes.inputInput,
-              }}
-              inputProps={{ 'aria-label': 'search' }}
-            />
-          </div>
+          <SearchField />
         </Toolbar>
       </Container>
     </AppBar>
---
+++ components/organisms/SearchField.tsx
@@ -0,0 +1,68 @@
+import { alpha, createStyles, InputBase, makeStyles } from '@material-ui/core'
+import React from 'react'
+import { Search } from '@material-ui/icons'
+
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    search: {
+      position: 'relative',
+      borderRadius: theme.shape.borderRadius,
+      backgroundColor: alpha(theme.palette.common.white, 0.15),
+      '&:hover': {
+        backgroundColor: alpha(theme.palette.common.white, 0.25),
+      },
+      marginLeft: 0,
+      width: '100%',
+      [theme.breakpoints.up('sm')]: {
+        marginLeft: theme.spacing(1),
+        width: 'auto',
+      },
+    },
+    searchIcon: {
+      padding: theme.spacing(0, 1),
+      height: '100%',
+      position: 'absolute',
+      pointerEvents: 'none',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    inputRoot: {
+      color: 'inherit',
+    },
+    inputInput: {
+      padding: theme.spacing(1, 1, 1, 0),
+      paddingLeft: `calc(1em + ${theme.spacing(3)}px)`,
+      transition: theme.transitions.create('width'),
+      width: '100%',
+      [theme.breakpoints.up('sm')]: {
+        width: '20ch',
+        '&:focus': {
+          width: '20ch',
+        },
+      },
+    },
+  }),
+)
+
+function SearchField() {
+  const classes = useStyles()
+
+  return (
+    <div className={classes.search}>
+      <div className={classes.searchIcon}>
+        <Search />
+      </div>
+      <InputBase
+        placeholder="検索"
+        classes={{
+          root: classes.inputRoot,
+          input: classes.inputInput,
+        }}
+        inputProps={{ 'aria-label': 'search' }}
+      />
+    </div>
+  )
+}
+
+export default SearchField
